55a14346696022e1b170d8a6f74641318d411af0
Ties <tiestienhoven@gmail.com>
2020-02-05 10:50:42 +0100

    Opdracht 1 gemaakt

--- MicroControllers/MicroControllers/main.c
+++ MicroControllers/MicroControllers/main.c
@@ -4,15 +4,28 @@
  * Created: 5-2-2020 10:44:24
  * Author : Ties
  */ 
+#define F_CPU 8000000
 
 #include <avr/io.h>
+#include <util/delay.h>
+
+void Move_Delay() {
+	_delay_ms(500);
+}
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+    opdracht1();
+}
+
+void opdracht1(){
+	 DDRD = 0xFF;
+	 while (1){
+		 PORTD = 0x04;
+		 Move_Delay();
+		 PORTD = 0x02;
+		 Move_Delay();
+	 }
 }
 
