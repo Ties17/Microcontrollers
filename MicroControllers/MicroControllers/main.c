046df8178fe2ca8034e398b30a797bee61e98771
Ties <tiestienhoven@gmail.com>
2020-02-05 10:44:55 +0100

    Project Created

---
+++ MicroControllers/MicroControllers/main.c
@@ -0,0 +1,18 @@
+/*
+ * MicroControllers.c
+ *
+ * Created: 5-2-2020 10:44:24
+ * Author : Ties
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
